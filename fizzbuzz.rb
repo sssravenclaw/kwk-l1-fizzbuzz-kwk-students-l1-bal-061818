0ba8d4ce4d9a6bd992753373b67349a6aa69bacb
Siobhan <yuriamisaki18@gmail.com>
2018-06-19 20:35:16 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,7 @@
+def fizz_buzz (num)
+  #if # mod 3 equal to 0
+  if num %3 == 0 
+    puts "fizz"
+  elsif num %5 == 0
+  
+end
---
+++ reading_arrays.rb
@@ -0,0 +1,15 @@
+students = ["allison", "lily", "hadja", "siobhan", "anu"]
+ puts students [2] = "cassidy"
+puts students [2]
+
+students [4] = "megan"
+puts students [4]
+
+students.push("grace")
+puts students
+
+students.push ("emma")
+puts students
+
+students.push ("shay")
+puts students
